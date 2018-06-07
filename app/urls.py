c5716641ce445e377c3b84c3b0feeb6b038f4593
Zull14 <36945682+Zull14@users.noreply.github.com>
2018-06-07 13:38:03 -0500

    DS

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('guardaLibro',views.guardaLibro),
+    path('libro',views.libro),
     path('guardarlector',views.guardarlector),
     path('lector',views.lector),
     path('visita/consulta', views.consultaVisita),
--- app/views.py
+++ app/views.py
@@ -16,6 +16,9 @@ def visita(request):
 def lector(request):
     return render(request, 'lector.html')
 
+def libro(request):
+    return render(request, 'ag_lib.html')
+
 def guardaVisita(request):
     if 'nombre' in request.POST and 'fecha' in request.POST and 'telefono' in request.POST and 'direccion' in request.POST:
         nombre = request.POST['nombre']
@@ -49,3 +52,19 @@ def guardarlector(request):
         return render (request,'lector.html',{'msg': 'Registro realizado exitosamente'})
     else:
         return render(request, 'lector.html', {'msg': 'No se puede realizar el registro'})
+
+def guardaLibro(request):
+    if 'isbn' in request.POST and 'titulo' in request.POST and 'autor' in request.POST and 'editorial' in request.POST and 'edicion' in request.POST and 'fimp' in request.POST and 'tipo' in request.POST:
+        isbn = request.POST['isbn']
+        titulo = request.POST['titulo']
+        autor = request.POST['autor']
+        editorial = request.POST['editorial']
+        edicion = request.POST['edicion']
+        fimp = request.POST['fimp']
+        tipo = request.POST['tipo']
+
+        p = models.libros(isbn = isbn, titulo = titulo, autor = autor,editorial = editorial,edicion = edicion,fimp = fimp,tipo = tipo)
+        p.save()
+        return render (request,'ag_lib.html',{'msg': 'Registro realizado exitosamente'})
+    else:
+        return render(request, 'ag_lib.html', {'msg': 'No se puede realizar el registro'})
