b357faabd42d2a24d3fda422ac60e9eaa60a692c
JValdespino <36280931+JValdespino@users.noreply.github.com>
2018-06-07 01:55:57 -0500

    JV

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('guardaVisita', views.guardaVisita),
     path('visita', views.visita),
     path('', views.index),
 ]
--- app/views.py
+++ app/views.py
@@ -12,3 +12,17 @@ def index(request):
 
 def visita(request):
     return render(request, 'visita.html')
+
+def guardaVisita(request):
+    if 'idVisita' in request.POST and 'nombre' in request.POST and 'fecha' in request.POST and 'telefono' in request.POST and 'direccion' in request.POST:
+        idVisita = request.POST['idVisita']
+        nombre = request.POST['nombre']
+        fecha = request.POST['fecha']
+        telefono = request.POST['telefono']
+        direccion = request.POST['direccion']
+
+        p = models.visita(idVisita = idVisita,nombre = nombre,fecha = fecha, telefono = telefono, direccion = direccion)
+        p.save()
+        return render (request,'visita.html',{'msg': 'Registro realizado exitosamente'})
+    else:
+        return render(request, 'visita.html', {'msg': 'No se puede realizar el registro'})
