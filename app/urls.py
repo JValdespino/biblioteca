c928b672e66a5d18aefafceb22b2c90d98ab38ac
JValdespino <36280931+JValdespino@users.noreply.github.com>
2018-06-11 12:24:03 -0500

    JV

--- app/urls.py
+++ app/urls.py
@@ -4,23 +4,20 @@ from . import views
 
 urlpatterns = [
     path('guardaVisita', views.guardaVisita),
+    path('visita/editarV', views.editaVisita),
+    path('visita/regVisita', views.regVisita),
+    path('regMenu', views.regresaMenu),
+    path('visita/modificarVisita', views.modificaVisita),
     path('visita/consulta', views.consultaVisita),
     path('visita', views.visita),
     path('visita/elimi', views.elimi),
-
+    
 
     path('guardaLibro',views.guardaLibro),
-    path('libro',views.libro),
-    path('guardarlector',views.guardarlector),
     path('lector',views.lector),
-    path('consultaInt', views.consultas_int),
     path('prestamos',views.prestamos),
     path('guardarprestamos',views.guardarprestamos),
     path('guardarlector',views.guardarlector),
-    path('lector',views.lector),
-    path('visita/consulta', views.consultaVisita),
-    path('guardaVisita', views.guardaVisita),
-    path('visita', views.visita),
     #path('consultaInt', views.consultas_int),
     path('', views.index),
     
